548852dab25e87446711ddbaf53e9a2dea3942d4
mondayCoding <moriluxuria@gmail.com>
2019-10-07 02:39:41 +0300

    olpå

--- src/Pages/Registration/Sections/QuestionSectionSelect.tsx
+++ src/Pages/Registration/Sections/QuestionSectionSelect.tsx
@@ -28,6 +28,14 @@ export const SelectQuestionSection: FC<IQuestionSectionSelectProps> = (props) =>
 			}))}
 		></SelectFieldBase>
 
+		{
+			// TODO: conditional rakenne
+			// task 1 ehdollinen / ei ehdollinen
+			// task 2 kun mikä valittu
+			// task 3 mikä on seuraava lohko
+			props.content.condition
+		}
+
 		<EditSectionBar isInEditMode={props.helpers.isInEditMode} helpers={props.helpers} />
 	</section>
 );
